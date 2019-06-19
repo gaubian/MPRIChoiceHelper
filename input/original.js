7578ac6bf049323512b7d55f12c7a50dd2375578
gaubian <guillaume.aubian@gaubian.xyz>
2019-06-19 22:08:59 +0200

    Updated site

--- input/original.js
+++ input/original.js
@@ -1,3 +1,61 @@
+const n = codes.length;
+
+function updateRight() {
+    const tab = document.querySelectorAll('selectable-row');
+    const areselected = new Array(n);
+    const deg = new Array(n);
+    const tak = new Array(n);
+    for(var i = 0; i < n; i++) {
+        deg[i] = 0;
+	tak[i] = 0;
+	areselected[i] = tab[i].isselected;
+    }
+    for(var i = 0; i < edges.length; i++) {
+	const u = edges[i][0];
+	const v = edges[i][1];
+	deg[u] += 1;
+	deg[v] += 1;
+        if(areselected[u] || areselected[v]) {
+	    tak[u] += 1;
+	    tak[v] += 1;
+	}
+    }
+    const to_sort = new Array(0);
+    for(var i = 0; i < n; i++) {
+	if(!areselected[i] && deg[i] > 0) {
+            to_sort.push([tak[i]/deg[i],i]);
+	}
+    }
+    to_sort.sort();
+    var ul = document.getElementById("rightcolumn");
+    ul.innerHTML = '';
+    for(var i = 0; i < 10 && to_sort.length - 1 - i >= 0; i++) {
+	var li = document.createElement("tr");
+	const ii = to_sort[to_sort.length - i - 1][1];
+	const x = Math.floor(to_sort[to_sort.length - i - 1][0] * 100);
+	var firstcell = document.createElement("td");
+	var secondcell = document.createElement("td");
+	var thirdcell = document.createElement("td");
+	firstcell.setAttribute("id", "firstcell");
+	firstcell.innerHTML = x + ' %';
+	secondcell.innerHTML = codes[ii];
+	secondcell.setAttribute("id", "secondcell");
+	thirdcell.innerHTML = names[ii]; 
+	li.appendChild(firstcell);
+	li.appendChild(secondcell);
+	li.appendChild(thirdcell);
+        ul.appendChild(li);
+    }
+    var ref_ects = document.getElementById("ECTS");
+    var ects = 0;
+    for(var i = 0; i < n; ++i) {
+	if(areselected[i]) {
+	    ects += 12 - 3 * codes[i].split('.').length;
+	}
+    }
+    ref_ects.innerHTML = "Nombre d'ECTS : " +  ects;
+}
+
 class SelectableRow extends HTMLElement {	
     get isselected() {
 	return this.hasAttribute('isselected');
@@ -10,6 +68,7 @@ class SelectableRow extends HTMLElement {
 	else {
 	    this.removeAttribute('isselected');
 	}
+	updateRight();
     }
 
     get index() {
@@ -34,7 +93,6 @@ class SelectableRow extends HTMLElement {
 
 customElements.define('selectable-row', SelectableRow);
 
-const n = codes.length;
 var ref_table = document.getElementById("table");
 var template = document.querySelector("#row");
 for(var i = 0; i < n; i++) {
@@ -47,3 +105,4 @@ const tab = document.querySelectorAll('selectable-row');
 for(var i = 0; i < n; i++) {
     tab[i].index(i);
 }
+updateRight();
